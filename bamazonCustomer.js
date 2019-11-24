8f80b852d0d65cb0f5b325813e0722317518bdc4
Jerrica VanAlstyne <jsnow07@yahoo.com>
2019-11-23 21:51:40 -0500

    added inquirer prompts, still to check if has amount of product and order of process

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,5 +1,6 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+const cTable = require('console.table');
 
 var connection = mysql.createConnection({
     host: "localhost",
@@ -11,22 +12,47 @@ var connection = mysql.createConnection({
     user: "root",
   
     // Your password
-    password: "",
+    password: "Olivia14!",
     database: "bamazon"
   });
 
   connection.connect(function (err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId + "\n");
-    displayAllItems();
-    connection.end();
+    // displayAllItems();
+    buyProduct()
+    
   });
 
   function displayAllItems(){
       connection.query(
           "SELECT * FROM products", function(err, results){
               if(err) throw err;
-              console.log(results)
+            //   console.log(results)
+              console.table(results);
+            
           }
       )
   }
+function buyProduct(){
+inquirer.prompt([
+    {
+        type:"input",
+        message:"What you like to purchase?",
+        name: "productName"        
+    }
+]).then(function(user){
+ var chosenProduct = user.productName
+ console.log(chosenProduct);
+ inquirer.prompt([
+   {
+     type: "input",
+     message:"How many would you like ot purchase?",
+     name:"quantity"
+   }
+ ]).then(function(requested){
+  var requestedQuantity = requested.quantity
+  console.log(requestedQuantity);
+ })
+});
+}
