8520a9180298102bc1d046727b80d3bc7591da8b
Jerrica VanAlstyne <jsnow07@yahoo.com>
2019-11-29 22:17:43 -0500

    updated add inventory function

--- bamazonManager.js
+++ bamazonManager.js
@@ -71,15 +71,72 @@ function viewProducts() {
 function lowInventory() {
     connection.query("SELECT * FROM products WHERE stock_quantity BETWEEN 0 AND 5", function (err, results) {
         if (err) throw err;
-        console.table(results);
-        managerFunction();
+        if (results.length > 0) {
+            console.log("-------------------------------");
+            console.table("These products have less than 5 items in stock: " + results);
+            console.log("-------------------------------");
+            managerFunction();
+        } else {
+            console.log("-------------------------------");
+            console.log("All products have more than 5 items in stock.");
+            console.log("-------------------------------");
+            managerFunction();
+        }
+
     });
 }
 
 function addInventory() {
-    
+    connection.query("SELECT item_id, product_name, price, stock_quantity FROM products", function (err, results) {
+        if (err) throw err;
+        // console.log(results)
+        // Create a new table object with the headers from MySql
+        console.table(results);
+    });
+    connection.query("SELECT * FROM products", function (err, results) {
+        if (err) throw err;
+
+        inquirer.prompt([
+            {
+                type: "checkbox",
+                message: "What product would you like to update the inventory?",
+                name: "selectProduct",
+                choices: function () {
+                    var choiceArr = []
 
 
+                    for (var i = 0; i < results.length; i++) {
+                        // console.log(results[i]);
+                        choiceArr.push(results[i].item_id);
+                    }
+                    return choiceArr;
+                }
+            },
+            {
+                type: "input",
+                message: "How much inventory would you like to add?",
+                name: "addInventory"
+            }
+        ]).then(function (supervisor) {
+            var updatedStock = (parseInt(supervisor.addInventory)) + (parseInt(results[supervisor.selectProduct - 1].stock_quantity));
+            console.log(updatedStock);
+            connection.query("UPDATE products SET ? WHERE ?",[
+                {
+                    stock_quantity: updatedStock,
+
+                },
+                {
+                    item_id: supervisor.selectProduct
+                }],
+                function (err, res) {
+                    if (err) throw err;
+                    console.log("Updated product inventory!")
+                    managerFunction();
+
+                }
+            )
+        })
+    })
 }
 
 function newProduct() {
@@ -119,7 +176,7 @@ function newProduct() {
             }
             , function (err, results) {
                 if (err) throw err;
-                console.table(newItem + " has been added to "+ newItemDepartment+".");
+                console.table(newItem + " has been added to " + newItemDepartment + ".");
                 managerFunction();
             });
     })
